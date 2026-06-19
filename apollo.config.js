49e26b8db8940beaad62f17f523f946483dcc898
Maximilian Uhlig <68818935+kaengurubeutel@users.noreply.github.com>
2026-06-19 10:04:23 +0200

    improved security

--- apollo.config.js
+++ apollo.config.js
@@ -1,10 +1,12 @@
 // apollo.config.js
+
+
 module.exports = {
   client: {
     service: {
       name: 'my-app',
       // URL to the GraphQL API
-      url: 'https://eu-west-2.cdn.hygraph.com/content/cmltyy9ly00ng07w27kip7jie/master/',
+      url: import.meta.env.VITE_API_KEY,
     },
     // Files processed by the extension
     includes: [
