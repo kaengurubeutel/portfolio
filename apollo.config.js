23a826b6b5d3201a5d1441cbe4cecfc29711ac83
Maximilian Uhlig <68818935+kaengurubeutel@users.noreply.github.com>
2026-06-19 10:09:03 +0200

    added API Key

--- apollo.config.js
+++ apollo.config.js
@@ -6,7 +6,7 @@ module.exports = {
     service: {
       name: 'my-app',
       // URL to the GraphQL API
-      url: import.meta.env.VITE_API_KEY,
+      url: 'https://eu-west-2.cdn.hygraph.com/content/cmltyy9ly00ng07w27kip7jie/master/',
     },
     // Files processed by the extension
     includes: [
